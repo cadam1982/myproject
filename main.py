9dda5b6daaa551482004f75617d99c6725d171a2
cadam1982 <christian_adam@gmx.at>
2026-02-14 17:53:47 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
-print("Hello GitHub")
+trainings = [
+    {"sport": "run", "duration": 45},
+    {"sport": "bike", "duration": 60},
+    {"sport": "swim", "duration": 20},
+]
+
+total_time = 0
+
+for session in trainings:
+    print(session)
+    total_time += session["duration"]
+
+
+print("Total training time:", total_time, "minutes")
